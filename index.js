303bcc6c3745a1b7f801b9c1da14e903ee0f7abf
noel.alam <noel.alam9999@gmail.com>
2023-09-18 20:32:13 +0600

    bug-fix:couldnt follow leads after 300th

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-const app = require('./app')
-const dev = require('./config/config')
+const { setFlagsFromString } = require("v8");
+const app = require("./app");
+const dev = require("./config/config");
+const Leads = require("./models/leads.model");
 const PORT = dev.app.port;
+const http = require("http").Server(app);
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://127.0.0.1:5173",
+  },
+});
 
+//Add this before the app.get() block
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on("message", async (data) => {
+    const lead = await Leads.findOne({
+      id: data.id,
+    });
+    console.log(lead);
+    lead.followerID = data.user_id;
+    lead.followerName = data.name;
+    await lead.save();
+  });
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+});
 
-app.listen(PORT, ()=>{
-    console.log(`server is running at port http://localhost:${PORT}`);
-})
+http.listen(PORT, () => {
+  console.log(`server is running at port http://localhost:${PORT}`);
+});
